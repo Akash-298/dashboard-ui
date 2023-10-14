5ea0c3fd2e7e4a02bc23a54bb0af72de4688dbb8
Akash-298 <akash.c.c298@gmail.com>
2023-10-14 17:36:42 +0530

    worked on the topbar

--- admin-ui-dashboard/src/App.jsx
+++ admin-ui-dashboard/src/App.jsx
@@ -1,16 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+import Topbar from "./scenes/global/Topbar";
 
 function App() {
- 
-  return (
-  
-      <div className="app">
-    </div>
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
 
-  )
+  return (
+    <>
+      <ColorModeContext.Provider value={colorMode}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <div className="app">
+            <Sidebar></Sidebar>
+            <main className="content">
+              <Topbar></Topbar>
+            </main>
+          </div>
+        </ThemeProvider>
+      </ColorModeContext.Provider>
+    </>
+  );
 }
 
-export default App
+export default App;
